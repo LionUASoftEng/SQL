c9c8a987dcc968e46d5e7ecccd1c1e5ef22936a3
John W <jwwolf@uark.edu>
2015-09-26 15:27:11 -0500

    Forget previous name change.
    I don't want to mess up what you have done in model.

--- create_employee_table.sql
+++ create_employee_table.sql
@@ -1,4 +1,4 @@
-create table employees
+create table employee
 (
 id 		uuid 			not null DEFAULT uuid_generate_v4(),
 firstname 	character varying(50) 	not null default ''::character varying,
@@ -6,7 +6,7 @@ lastname 	character varying(50) 	not null default ''::character varying,
 employeeid 	int 			not null default ((0)),
 active 		boolean 		not null default ((TRUE)),
 classification 	character(5),
-manager 	uuid 			references employees(id),
+manager 	uuid 			references employee(id),
 password 	character varying(50) 	default (''),
 createdon 	timestamp 		without time zone not null default now(),
 constraint 	employee_pkey 		primary key (id),
