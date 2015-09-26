accd28986f19583f4a44690820fee52e1efdf380
John W <jwwolf@uark.edu>
2015-09-26 15:24:48 -0500

    Changed table name
    I had been taught that table names should be plural so it is easily identified as a collection of data. In this case the table employees contains many employee records.  Since the transactions table has to reference the employee from the employees table as the cashier.

--- create_employee_table.sql
+++ create_employee_table.sql
@@ -1,4 +1,4 @@
-create table employee
+create table employees
 (
 id 		uuid 			not null DEFAULT uuid_generate_v4(),
 firstname 	character varying(50) 	not null default ''::character varying,
@@ -6,7 +6,7 @@ lastname 	character varying(50) 	not null default ''::character varying,
 employeeid 	int 			not null default ((0)),
 active 		boolean 		not null default ((TRUE)),
 classification 	character(5),
-manager 	uuid 			references employee(id),
+manager 	uuid 			references employees(id),
 password 	character varying(50) 	default (''),
 createdon 	timestamp 		without time zone not null default now(),
 constraint 	employee_pkey 		primary key (id),
